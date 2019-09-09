613850f4487683d857a442d2c138239462767063
mozieblo <m.ozieblo@wp.pl>
2019-09-09 12:07:49 +0200

    start expensify app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+
+ReactDOM.render(<p>Expensify!</p>, document.getElementById('app'));
+
+
