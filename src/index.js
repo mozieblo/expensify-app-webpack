3634a65db4e7c69fa6eab5421aac526cac239fa7
mozieblo <m.ozieblo@wp.pl>
2019-09-16 13:55:48 +0200

    divided folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
+
+const store = configureStore();
+
+const unsbuscribe = store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+})
+
+store.dispatch(addExpense({ description: 'water bill', amount: 500 }));
+store.dispatch(addExpense({ description: 'gas bill', amount: 130 }));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
