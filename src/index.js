443ea0079c4f5425be908b600c986aa68403c213
mozieblo <m.ozieblo@wp.pl>
2019-09-17 11:21:52 +0200

    react-redux provider and connect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -21,4 +22,14 @@ store.dispatch(addExpense({ description: 'gas bill', amount: 130 }));
 store.dispatch(setTextFilter('bill'));
 store.dispatch(setTextFilter('water'));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'));
+}, 3000);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
