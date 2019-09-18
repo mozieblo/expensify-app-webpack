7744ca8832452eb939916ae6ed7b282673b7caea
mozieblo <m.ozieblo@wp.pl>
2019-09-18 14:07:47 +0200

    update

--- src/components/ExpenseFrom.js
+++ src/components/ExpenseFrom.js
@@ -8,14 +8,17 @@ import 'react-dates/initialize';
 // console.log(now.format('DD MMMM YYYY'));
 
 export default class ExpenseForm extends React.Component {
-    state = {
-            description: '',
-            note: '',
-            amount: '',
-            createdAt: moment(),
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
             calendarFocused: false,
             error: ''
-    };
+        };
+    }
     
     onDescriptionChange = (e) => {
         const description = e.target.value;
