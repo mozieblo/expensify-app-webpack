0a4ec624b944e31a619d11bcf5cb8afdd54c3059
mozieblo <m.ozieblo@wp.pl>
2019-09-18 10:56:40 +0200

    react-date calednar Picker

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseFrom';
 
 const AddExpensePage = () => (
     <div>
-        This is from my add expense!
+        <h1>Add Expense</h1>
+        <ExpenseForm />
     </div>
 );
 
