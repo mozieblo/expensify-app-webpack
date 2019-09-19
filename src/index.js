5afdc42559863998ba3494b2b7e8cbec1f9400b3
mozieblo <m.ozieblo@wp.pl>
2019-09-19 10:08:12 +0200

    calendar Picker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
@@ -14,7 +13,6 @@ const store = configureStore();
 const unsbuscribe = store.subscribe(() => {
     const state = store.getState();
     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
 })
 
 store.dispatch(addExpense({ description: 'water bill', amount: 5000 }));
